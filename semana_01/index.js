1712d037d45a10fd18efb8bd790cbb4ae603e8c3
RosarioAgostinaBurs <rosario.burs@davinci.edu.ar>
2025-08-17 16:46:28 -0300

    Update index.js

--- semana_01/index.js
+++ semana_01/index.js
@@ -1,1 +1,20 @@
-// Ejercicio 01
+const Alumno = require('./Alumno.js');
+
+const a1 = new Alumno('Rosario', 'Burs', 25, 'DPW');
+
+const carrera = a1.obtenerCarrera();
+console.log(carrera);
+
+const datos = a1.obtenerDatos();
+console.log(datos);
+
+a1.modificarEdad(26);
+
+a1.agregarMateria('Aplicaciones Híbridas');
+a1.agregarMateria('IoT');
+
+const materias = a1.mostrarMaterias();
+console.table(materias);
+
+const edad = a1.retornarEdad();
+console.log(edad);
