15a1504e43b853a325a59aa6daa8139be857f30b
RosarioAgostinaBurs <rosario.burs@davinci.edu.ar>
2025-08-23 13:30:05 -0300

    Update index.js

--- semana_01/ejercicio_2/index.js
+++ semana_01/ejercicio_2/index.js
@@ -1,1 +1,17 @@
+const ProductManager = require('./ProductManager.js');
+const p1 = new ProductManager();
 
+async function guardarProduct() {
+    await p1.addProduct('Celular');
+    await p1.addProduct('Notebook');
+}
+
+async function main() {
+    await guardarProduct();
+    console.log('✅ Productos guardados');
+
+    const data = await p1.getProducts();
+    console.table(data);
+}
+
+main();
